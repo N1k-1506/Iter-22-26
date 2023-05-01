ad385388b332fcca3005058e5b1868f1326031f3
N1k-1506 <125110633+N1k-1506@users.noreply.github.com>
2023-05-01 21:31:06 +0530

    Update q01.java

--- Sem 2/Assignment01/q01.java
+++ Sem 2/Assignment01/q01.java
@@ -1,3 +1,6 @@
+/*Write a Java program that can take a positive integer greater than 2 as input and write out the
+number of times one must repeatedly divide this number by 2 before getting a value less
+than 2.*/
 import java.util.Scanner;
 public class q01 {
 
