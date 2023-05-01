e99fe7245e79a8fe8a70984aacca825428d18d3c
N1k-1506 <125110633+N1k-1506@users.noreply.github.com>
2023-05-01 21:33:35 +0530

    Update q03.java

--- Sem 2/Assignment01/q03.java
+++ Sem 2/Assignment01/q03.java
@@ -1,3 +1,15 @@
+/*Write a java program to check whether a number is a spy number or not.
+
+Example:
+N = 132
+Sum of digit of N = 1 + 3 + 2 = 6
+Product of digit of N = 1 × 3 × 2 = 6
+So, 132 is a spy number.
+
+Sample Run:
+Enter a number: 1124
+1124 is a spy number.*/
+
 import java.util.Scanner;
 public class q03 {
 
