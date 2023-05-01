30e0c2a5536937c3f1c01dc2a69b835cd9df385f
N1k-1506 <125110633+N1k-1506@users.noreply.github.com>
2023-05-01 21:41:23 +0530

    Recently updated..
    This code is done using recursion.

--- Sem 2/Assignment01/q04.java
+++ Sem 2/Assignment01/q04.java
@@ -1,31 +1,29 @@
 
-public class q04 {
 
-		// TODO Auto-generated method stub
-	/*	static String swap(String str,int i, int j){
-			char a[]=str.toCharArray();
-			char temp=a[i];
-			a[i]=a[j];
-			a[j]=temp;		
-		}
-		static String[] Permute(String[] str,int start,int end) {
-			if(start==end) {
-				return str;
-			}			
-			String newstr=swap(str[start],start+1,end);
-			newstr=swap(str[start+1],start+1,end-1);
-			return newstr;
-		}
-		public static void main(String args[]) {
-			String[] a= {"c","a","r"};
-			for(int i=0;i<a.length;i++) {
-				String str=a[i];
-				
-				
-			}
-			
-			
-			
-		}*/
-	
+public class q04 {
+    static char[] characters = {'c','a','r','b','o','n'};
+    static char[] charac= {'c','a','r'};
+    public static void main(String[] args) {
+        printPermutation(charac,0, charac.length);
+    }
+   
+    private static void printPermutation(char[] a, int startIndex, int endIndex) {
+        if (startIndex == endIndex)//reached end of recursion, print the state of a
+            System.out.println(new String(a));
+        else {
+            //try to move the swap window from start index to end index
+            //i.e 0 to a.length-1
+            for (int x = startIndex; x < endIndex; x++) {
+                swap(a, startIndex, x);
+                printPermutation(a, startIndex + 1, endIndex);
+                swap(a, startIndex, x);
+            }
+        }
+    }
+   
+    private static void swap(char[] a, int i, int x) {
+        char t = a[i];
+        a[i] = a[x];
+        a[x] = t;
+    }
 }
