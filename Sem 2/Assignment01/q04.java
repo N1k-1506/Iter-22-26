da05ca44520702ea906c503c813751299d440518
N1k-1506 <125110633+N1k-1506@users.noreply.github.com>
2023-05-01 21:36:51 +0530

    Update q04.java

--- Sem 2/Assignment01/q04.java
+++ Sem 2/Assignment01/q04.java
@@ -1,31 +1,26 @@
 
 public class q04 {
-
-		// TODO Auto-generated method stub
-	/*	static String swap(String str,int i, int j){
-			char a[]=str.toCharArray();
-			char temp=a[i];
-			a[i]=a[j];
-			a[j]=temp;		
+	static String swap(String str,int i, int j){
+		char a[]=str.toCharArray();
+		char temp=a[i];
+		a[i]=a[j];
+		a[j]=temp;		
+	}
+	static String[] Permute(String[] str,int start,int end) {
+		if(start==end) {
+			return str;
+		}			
+		String newstr=swap(str[start],start+1,end);
+		newstr=swap(str[start+1],start+1,end-1);
+		return newstr;
 		}
-		static String[] Permute(String[] str,int start,int end) {
-			if(start==end) {
-				return str;
-			}			
-			String newstr=swap(str[start],start+1,end);
-			newstr=swap(str[start+1],start+1,end-1);
-			return newstr;
-		}
-		public static void main(String args[]) {
-			String[] a= {"c","a","r"};
-			for(int i=0;i<a.length;i++) {
-				String str=a[i];
+	public static void main(String args[]) {
+		String[] a= {"c","a","r"};
+		for(int i=0;i<a.length;i++) {
+			String str=a[i];
 				
 				
-			}
+	}
 			
 			
-			
-		}*/
-	
 }
