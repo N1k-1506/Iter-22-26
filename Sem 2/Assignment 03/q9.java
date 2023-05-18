ea8cc7291c5f264ed770638d3d7bf4d25adfe3f2
Nikhil <125110633+N1k-1506@users.noreply.github.com>
2023-05-19 01:51:57 +0530

    Upload q9
    Question 9 of Assignment 3 was uploaded.

---
+++ Sem 2/Assignment 03/q9.java
@@ -0,0 +1,18 @@
+package Dsa_03;
+import java.util.Scanner;
+public class q9 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter value of n: ");
+        int n=sc.nextInt();
+        int x=fibonacci((n-2), 1, 1);
+        System.out.println("The "+n+" term in fibonacci is "+x);
+    }
+    public static int fibonacci(int n,int a,int b) {
+        if(n==0){
+            return b;
+        }
+        int c=a+b;
+        return fibonacci(n-1, b, c);
+    }
+}
