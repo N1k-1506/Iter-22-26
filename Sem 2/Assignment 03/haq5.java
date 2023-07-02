8024beaa61c00b33615645824b1a262de09d9568
Nikhil <125110633+N1k-1506@users.noreply.github.com>
2023-07-03 01:50:20 +0530

    haq5

--- Sem 2/Assignment 03/haq5.java
+++ Sem 2/Assignment 03/haq5.java
@@ -1,3 +1,15 @@
 public class haq5 {
-    
+    public static void main(String[] args) {
+        int n=3;
+        towerofhanoi(n, "A", "B", "C");
+    }
+    public static void towerofhanoi(int n,String src,String help,String dest){
+        if(n==1){
+            System.out.println("transfer disk "+n+" from tower "+src+" to tower"+dest);
+            return;
+        }
+        towerofhanoi(n-1, src, dest, help);
+        System.out.println("transfer disk "+n+" from tower "+src+" to tower"+dest);
+        towerofhanoi(n-1, help, src, dest);
+    }
 }
