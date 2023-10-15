305687c0e8cce9a50cd0b8f21d61df88ac89b424
Nikhil <125110633+N1k-1506@users.noreply.github.com>
2023-10-15 23:12:54 +0530

    Update q08.java

--- Sem3/Asssignment_02/q08.java
+++ Sem3/Asssignment_02/q08.java
@@ -1,20 +1,20 @@
-package Asssignment_02;
-import java.util.Arrays;
-public class q08 {
-    public static void main(String[] args) {
-       int[] a={-8,3,-4,0,2,1,-1};
-        Arrays.sort(a); 
-        int[] temp=positive(a, 0, 0);
-    }
-    static int[] positive(int[] a,int i,int j){
-        int[] temp=new int[a.length];
-        if(i==(a.length-1))
-            return temp;
-        if(a[i]>0){
-            temp[j]=a[i];
-            j=j+1;
-        }
-        positive(a, (i+1), j);
-        return temp;
-    }
-}
+// package Asssignment_02;
+// import java.util.Arrays;
+// public class q08 {
+//     public static void main(String[] args) {
+//        int[] a={-8,3,-4,0,2,1,-1};
+//         Arrays.sort(a); 
+//         int[] temp=positive(a, 0, 0);
+//     }
+//     static int[] positive(int[] a,int i,int j){
+//         int[] temp=new int[a.length];
+//         if(i==(a.length-1))
+//             return temp;
+//         if(a[i]>0){
+//             temp[j]=a[i];
+//             j=j+1;
+//         }
+//         positive(a, (i+1), j);
+//         return temp;
+//     }
+//}
