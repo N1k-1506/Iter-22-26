1c6f7cfbee2de4433f1d650a61c8ae1ac6dced0d
Nikhil <125110633+N1k-1506@users.noreply.github.com>
2023-10-27 11:07:29 +0530

    Update q04.java

--- Sem3/Assignment_02/q04.java
+++ Sem3/Assignment_02/q04.java
@@ -1,13 +1,11 @@
 package Asssignment_02;
 public class q04 {
     public static void main(String[] args) {
-        System.out.println(fibo(10, 0, 1));
+        System.out.println(fibo(10));
     }
-    static int fibo(int n,int a,int b){
-        if(n==0)
-            return a;
-        if(n==1)
-            return b;
-        return fibo((n-1),b,(b+a));
+    static int fibo(int n){
+        if(n<=1)
+            return n;
+        return fibo(n-1)+fibo(n-2);
     }
 }
